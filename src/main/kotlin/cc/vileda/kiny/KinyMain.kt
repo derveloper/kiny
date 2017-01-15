ea5f66eda7b139e83abdeb94b6d0a74d7f93e8fb
derveloper <vileda+github@vileda.cc>
2017-01-15 23:49:58 +0100

    add user feedback in web console

--- src/main/kotlin/cc/vileda/kiny/KinyMain.kt
+++ src/main/kotlin/cc/vileda/kiny/KinyMain.kt
@@ -118,7 +118,14 @@ class EndpointVerticle(private val appDef: AppDef) : AbstractVerticle() {
         val eventBus = vertx.eventBus()
         consumer = eventBus.consumer<String>("trigger.request.${deploymentID()}") {
             logger.info("(${deploymentID()}) received ${it.body()}")
-            it.reply(appDef.invoke(JsonObject(it.body())))
+            val message = it
+            vertx.executeBlocking<JsonObject>({ fut ->
+                vertx.setTimer(10000, { fut.fail(JsonObject().put("error", "timeout").encode()) })
+                fut.complete(appDef.invoke(JsonObject(message.body())))
+            }, {
+                if (it.succeeded()) message.reply(it.result())
+                else message.reply(JsonObject(it.cause().message))
+            })
         }
     }
 
